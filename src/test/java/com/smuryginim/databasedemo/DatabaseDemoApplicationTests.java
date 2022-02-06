ed56216552bb377fc05e9534c0061447ff876d09
IvanSmurygin <18061399eab0054680fbfa5d189200168610f42e>
2022-02-06 14:52:41 +0100

    - initial commit

---
+++ src/test/java/com/smuryginim/databasedemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,1 @@
+package com.smuryginim.databasedemo;import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;@SpringBootTestclass DatabaseDemoApplicationTests {	@Test	void contextLoads() {	}}
