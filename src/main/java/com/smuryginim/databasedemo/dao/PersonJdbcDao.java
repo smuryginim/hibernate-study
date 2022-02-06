709b125333346dd5965ad9405bb60cd50560f358
IvanSmurygin <18061399eab0054680fbfa5d189200168610f42e>
2022-02-06 15:12:34 +0100

    - change line separators

--- src/main/java/com/smuryginim/databasedemo/dao/PersonJdbcDao.java
+++ src/main/java/com/smuryginim/databasedemo/dao/PersonJdbcDao.java
@@ -1,1 +1,21 @@
-package com.smuryginim.databasedemo.dao;import com.smuryginim.databasedemo.model.DbPerson;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.jdbc.core.BeanPropertyRowMapper;import org.springframework.jdbc.core.JdbcTemplate;import org.springframework.stereotype.Repository;import java.util.List;@Repositorypublic class PersonJdbcDao {    @Autowired    public JdbcTemplate jdbcTemplate;    public List<DbPerson> findAll() {        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(DbPerson.class));    }}
+package com.smuryginim.databasedemo.dao;
+
+import com.smuryginim.databasedemo.model.DbPerson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    public JdbcTemplate jdbcTemplate;
+
+    public List<DbPerson> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(DbPerson.class));
+    }
+
+}
--- src/test/java/com/smuryginim/databasedemo/DatabaseDemoApplicationTests.java
+++ src/test/java/com/smuryginim/databasedemo/DatabaseDemoApplicationTests.java
@@ -1,1 +1,15 @@
-package com.smuryginim.databasedemo;import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;@SpringBootTestclass DatabaseDemoApplicationTests {	@Test	void contextLoads() {	}}
+package com.smuryginim.databasedemo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
